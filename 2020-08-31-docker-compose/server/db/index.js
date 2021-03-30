bdeea3bb0593a8e06f953ec5d086eef2f3db681d
Ismail ALABOU <alabou.ismail@gmail.com>
2021-03-30 17:38:48 +0100

    fix a bunch of docker container build issues.

--- 2020-08-31-docker-compose/server/db/index.js
+++ 2020-08-31-docker-compose/server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://mongo:27017/cinema';
+const connectionString = 'mongodb+srv://ismailalabou:ouhY5iSFjUd2guFs@cluster0.s2lzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
   console.error('Connection error', e.message);
